1355e4a6a633df3b3eac71901091504d2f5aa583
tjoko <take.ehime@gmail.com>
2020-11-25 17:55:40 +0900

    new, create を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+  #新規作成のアクション
+  def new
+    @message = Message.new
+  end
+
+
+  def create
+    Message.create(message_params)
+  end
+  #Strong Parameterの記述
+  private
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
+
+
 end
